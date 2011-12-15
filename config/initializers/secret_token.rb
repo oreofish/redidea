0bcc2f9449ab6048934cf4c70fe779360aa29be1
Sian Cao <sycao@redflag-linux.com>
2011-12-15 11:00:53 +0800

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redidea::Application.config.secret_token = '75228e84fc2692d027ed3e36c3445ca4942f9d55088dcf05d0df3aec74ecc26d58aa9a3c9bf96b19289d27314f3e0f2b2213899f19b2edd420f8ce4baf7bb971'
