6a81aea21cacd40e1ed994a2f44452d89b6d18f6
Rick Xing <jianxing@redflag-linux.com>
2011-12-16 16:15:53 +0800

    add rspec cases for idea module

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,45 @@
 require 'spec_helper'
 
 describe Idea do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { :user_id => 4, :title => "title", :content => "test content"}
+  end
+
+  it "should create a new idea with attr" do
+    Idea.create!(@attr)
+  end
+  
+  it "should not create without title" do 
+    no_title_idea = Idea.new(@attr.merge(:title => ""))
+    no_title_idea.should_not be_valid
+  end
+
+  it "should not create with long title" do 
+    long_title = "a" * 31
+    long_idea = Idea.new(@attr.merge(:title => long_title))
+    long_idea.should_not be_valid
+  end
+  
+  it "should not create without content" do 
+    no_content_idea = Idea.new(@attr.merge(:content => ""))
+    no_content_idea.should_not be_valid
+  end
+
+  it "should not create with too short content" do 
+    short_content = "a" * 5
+    short_idea = Idea.new(@attr.merge(:content => short_content))
+    short_idea.should_not be_valid
+  end
+
+  it "should not create with long content" do 
+    long_content = "a" * 401
+    long_idea = Idea.new(@attr.merge(:content => long_content))
+    long_idea.should_not be_valid
+  end
+
+  it "should not create idea with same title" do 
+    Idea.create!(@attr)
+    idea = Idea.new(@attr)
+    idea.should_not be_valid
+  end
 end
