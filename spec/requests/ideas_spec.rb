e9fe9186d5809519bb14fb9ccb82f4fd397fea68
Rick Xing <oreofish@gmail.com>
2011-12-17 17:03:13 +0800

    add User model, add login links in homepage and verify if it can work

--- spec/requests/ideas_spec.rb
+++ spec/requests/ideas_spec.rb
@@ -2,10 +2,28 @@ require 'spec_helper'
 
 describe "Ideas" do
   describe "GET /ideas" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    it "should have sign_in/sign_up links" do
       get ideas_path
-      response.status.should be(200)
+      response.should have_selector("a", :href => new_user_session_path,
+                                    :content => "Sign in")
+      response.should have_selector("a", :href => new_user_registration_path,
+                                    :content => "Sign up")
+    end
+    
+    describe "after logged in" do
+      before(:each) do
+        @user = Factory(:user)
+        visit new_user_session_path
+        fill_in :email,    :with => @user.email
+        fill_in :password, :with => @user.password
+        click_button
+      end
+
+      it "should have a signout link" do
+        get ideas_path
+        response.should have_selector("a", :href => destroy_user_session_path,
+                                      :content => "Sign out")
+      end
     end
   end
 end
