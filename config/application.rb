78418a594db156d6e8642f81ca0ad2e17cfb776b
Rick Xing <jianxing@redflag-linux.com>
2011-12-21 18:28:28 +0800

    update rules

--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,13 @@ module Redidea
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Beijing'  
+    config.active_record.default_timezone = :local  
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = "zh-CN".to_sym
+    I18n.default_locale = 'zh-CN'.to_sym
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
