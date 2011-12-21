f6a4c2317fe521bd42db327a8df8744737e665e3
Sian Cao <sycao@redflag-linux.com>
2011-12-21 17:00:35 +0800

    refine idea length counter
    monitor input event to catch all inputs (include IM input)

--- app/assets/javascripts/ideas_hints.js
+++ app/assets/javascripts/ideas_hints.js
@@ -1,16 +1,22 @@
 function bindIdeaHandler() {
     var $idea_content = $('#idea_content').first();
-    var $hint = $('.content .field .num').first();
-
-    var idea_validator = {
-        keyhandler : function(e) {
-            var $this = $(this);
-            $hint.find('span').html( "" + (400 - $this.attr('value').length) );
-        }
-    };
-
     if ($idea_content.length) {
-        $idea_content.bind( 'keypress', idea_validator.keyhandler );
+        var $hint = $('.content .field .num').first();
+
+        var idea_validator = {
+            keyhandler : function(e) {
+                var $this = $(this);
+                $hint.find('span').html( "" + (400 - $this.attr('value').length) );
+            }
+        };
+
+        $idea_content.bind({
+            //'keypress': idea_validator.keyhandler,
+            'input': idea_validator.keyhandler,
+            //'compositionend': idea_validator.keyhandler,
+            'focusin'   : function() { $hint.fadeIn('slow') },
+            'focusout' : function() { $hint.fadeOut('fast') },
+        });
     }
 
 }
