4574f6b9888a4f0f5e21087a6d1fd2e00eb97832
zhou <zhou@zhou.(none)>
2011-12-21 11:43:50 +0800

    send confirm mail

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'will_paginate'
 gem 'json'
 gem 'jquery-rails'
 gem 'devise'
+gem 'therubyracer'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -52,10 +51,14 @@ ActiveRecord::Schema.define(:version => 20111220075756) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
