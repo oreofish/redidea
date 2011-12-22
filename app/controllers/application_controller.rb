65c3bb1f09ebc9e7c1667461186fb1da2aecf525
Rick Xing <jianxing@redflag-linux.com>
2011-12-22 12:21:29 +0800

    add many <%# encoding: utf-8 %>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_charset
-  def set_charset
-    @headers["Content-Type"] = "text/html; charset=utf-8"
-  end
 end
