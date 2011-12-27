d3bbb956fd2050ee5d78e932b54d1e621a7e09e5
Sian Cao <sycao@redflag-linux.com>
2011-12-27 19:31:56 +0800

    fix ideas handler binding problem
    now all request send in AJAX way, so it's not enough to bind handlers
    when window is ready (onload). we also have to take care of siwtching of
    tabs.

--- app/assets/javascripts/ideas_hints.js
+++ app/assets/javascripts/ideas_hints.js
@@ -1,48 +1,56 @@
-function bindIdeaHandler() {
-    var $idea_content = $('#idea_content').first();
-    if ($idea_content.length) {
-        var $hint = $('.content .field .num').first();
+var ideasController = {
+    initialized: false, // this handler are binded, then it's true
 
-        var idea_validator = {
-            hotkeyHandler : function(e) {
-                if (e.ctrlKey && e.which == 13) {
-                    $('.new_idea :submit').first().trigger('click');
+    bindIdeaHandler: function() {
+        var $idea_content = $('#idea_content').first();
+        if ($idea_content.length) {
+            var $hint = $('.content .field .num').first();
+
+            var idea_validator = {
+                hotkeyHandler : function(e) {
+                    if (e.ctrlKey && e.which == 13) {
+                        $('.new_idea :submit').first().trigger('click');
+                    }
+                },
+
+                keyHandler : function(e) {
+                    var $this = $(this);
+                    $hint.find('span').html( "" + (400 - $this.attr('value').length) );
                 }
-            },
+            };
 
-            keyHandler : function(e) {
-                var $this = $(this);
-                $hint.find('span').html( "" + (400 - $this.attr('value').length) );
-            }
-        };
-
-        $idea_content.bind({
-            'keypress': idea_validator.hotkeyHandler,
-            'input': idea_validator.keyHandler,
-            'keyup': idea_validator.keyHandler,
-            //'compositionend': idea_validator.keyhandler,
-            'focusin'   : function() { $hint.fadeIn('slow') },
-            'focusout' : function() { $hint.fadeOut('fast') },
-        });
-    }
-
-}
-
-function handleIdeaCreate() {
-    var log_idea_create_success = false;
-    $('#new_idea').bind({
-        'ajax:failure': function() {
-        },
-        'ajax:success': function() {
-            log_idea_create_success = true;
-        },
-        'ajax:complete': function() {
-            log_idea_create_success = false;
+            $idea_content.bind({
+                'keypress': idea_validator.hotkeyHandler,
+                'input': idea_validator.keyHandler,
+                'keyup': idea_validator.keyHandler,
+                //'compositionend': idea_validator.keyhandler,
+                'focusin'   : function() { $hint.fadeIn('slow') },
+                'focusout' : function() { $hint.fadeOut('fast') },
+            });
+            this.initialized = true;
         }
-    });
-}
 
-$(document).ready( function() {
-    handleIdeaCreate(); 
-    bindIdeaHandler();
-} );
+    },
+
+    handleIdeaCreate: function() {
+        var log_idea_create_success = false;
+        $('#new_idea').bind({
+            'ajax:failure': function() {
+            },
+            'ajax:success': function() {
+                log_idea_create_success = true;
+            },
+            'ajax:complete': function() {
+                log_idea_create_success = false;
+            }
+        });
+    },
+
+    init: function() {
+        if (!this.initialized) {
+            this.handleIdeaCreate(); 
+            this.bindIdeaHandler();
+        }
+    } 
+};
+
