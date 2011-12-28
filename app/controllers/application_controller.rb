5da09f36af4148242b5549595d4f27881edf7dca
Rick Xing <jianxing@redflag-linux.com>
2011-12-28 13:31:04 +0800

    redirect to user edit page after confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  protected
+
+  # Overwriting the after confirmation redirect path method
+  def after_confirmation_path_for(resource_name, resource)
+    edit_user_registration_path
+  end
 end
