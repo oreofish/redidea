66313a266e9fb6360d71744bf9f0a01743357655
Rick Xing <oreofish@gmail.com>
2012-01-03 12:07:58 +0800

    show updated ideas in liked page, base on faye

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,39 @@
 //= require jquery-ui
 //= require_tree .
 $(function(){
-  var client = new Faye.Client('http://localhost:9292/faye');
-  client.subscribe("/messages/new",function(data){
-    eval(data);
-  });
+    var client = new Faye.Client('http://localhost:9292/faye');
+    client.subscribe("/messages/new",function(data){
+        eval(data);
+    });
+
+    // faye log
+    Logger = {
+        update_count: 0,
+
+        incoming: function(message, callback) {
+            if (message['channel'] == "/messages/new") {
+                var feedback = eval("(" + message['data'] + ")");
+                var user_email = $("#user-navigation .wat-cf li").eq(0).html();
+                if (feedback.user_email != user_email) {
+                    this.update_count += 1
+                    var link = "<a href='/ideas' class='notify'>更新了"
+                        + this.update_count + "个点子，点击查看</a>";
+                    $('div.update-notice').html(link);
+                }
+                console.log('incoming', user_email + this.update_count);
+            }else{
+                callback(message);
+            }
+        },
+        outgoing: function(message, callback) {
+            if (message['channel'] == "/messages/new") {
+                console.log('outgoing', message);
+            }else{
+                callback(message);
+            }
+        }
+    };
+
+    client.addExtension(Logger);
+
 });
