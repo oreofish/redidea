d9d4316c505bf4c95c55ca70393087b0d6e3ad6b
shensuwen <shensuwen@redflag-linux.com>
2012-01-04 16:30:00 +0800

    add email

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,7 +4,19 @@ class AdminsController < ApplicationController
 	# POST /ranks
 	def admin
 		if can? :rank, Idea
-			@ideas = Idea.find_by_sql("SELECT ideas.*,SUM(likes.score) AS scores,COUNT(likes.idea_id) AS counts From ideas,likes WHERE likes.idea_id=ideas.id GROUP BY likes.idea_id ORDER BY scores DESC")
+			@scope = params[:scope] || 'rank'
+			@scopes= [:rank, :email]
+			if @scope == "rank"
+				@ideas = Idea.find_by_sql("SELECT ideas.*,SUM(likes.score) AS scores,COUNT(likes.idea_id) AS counts From ideas,likes WHERE likes.idea_id=ideas.id GROUP BY likes.idea_id ORDER BY scores DESC")
+			elsif @scope == "email"
+				
+			end
+
+    	respond_to do |format|
+      	format.html # index.html.erb
+      	format.js
+    	end
+
 		else 
 			redirect_to ideas_path
 		end
